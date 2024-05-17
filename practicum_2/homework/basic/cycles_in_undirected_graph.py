ec53246db7a77bcea7ff9207118a13599528e51e
milashkarr <149618096+milashkarr@users.noreply.github.com>
2024-05-17 18:51:45 +0300

    Update and rename hw1_new.py to cycles_in_undirected_graph.py

---
+++ practicum_2/homework/basic/cycles_in_undirected_graph.py
@@ -0,0 +1,27 @@
+import os
+
+import networkx as nx
+
+
+TEST_GRAPH_FILES = [
+    "graph_1_wo_cycles.edgelist",
+    "graph_2_w_cycles.edgelist",
+]
+
+
+def has_cycles(g: nx.Graph):
+     cycles = nx.cycle_basis(G)
+     if len(cycles) > 0:  # Если метод возвращает непустой список, то цикл существует
+        return True
+     else:
+        return False
+
+
+if __name__ == "__main__":
+    for filename in TEST_GRAPH_FILES:
+        # Load the graph
+        G = nx.read_edgelist(
+            os.path.join("practicum_2", "homework", filename), create_using=nx.Graph
+        )
+        # Output whether it has cycles
+        print(f"Graph {filename} has cycles: {has_cycles(G)}")
