80e0f7d775a7942227a86d8265e72df072bb9418
milashkarr <149618096+milashkarr@users.noreply.github.com>
2024-05-18 19:11:45 +0300

    Update inverse_power_method.py

--- practicum_7/homework/basic/inverse_power_method.py
+++ practicum_7/homework/basic/inverse_power_method.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.common import NDArrayFloat
+
+from numpy.typing import NDArray
+NDArrayFloat = NDArray[np.float_]
+
 
 
 def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
@@ -11,12 +14,12 @@ def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
     history = []  # Список для сохранения истории минимальных собственных чисел
 
     for _ in range(n_iters):  # Следующий вектор
-        pre_u_kk = np.linalg.solve(A, u_k)
+        pre_u_kk = np.linalg.solve(A, u_k)  # Решаем систему линейных уравнений Ax = u_k => x = A^(-1) * u_k
         A_u_k_norm = np.linalg.norm(pre_u_kk)
         u_k = pre_u_kk / A_u_k_norm
 
         # Вычисляем приближение к наименьшему по модулю собственному числу матрицы A
-        eigenvalue = np.linalg.norm(np.dot(A, u_k)) / np.linalg.norm(u_k)
+        eigenvalue = np.linalg.norm(np.dot(A, u_k)) / np.linalg.norm(u_k)  # ||Au_k|| / ||u_k||
         history.append(eigenvalue)
 
     return history
