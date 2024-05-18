11b954aef0e89634bf7a85768ad9d87b93f1079c
milashkarr <149618096+milashkarr@users.noreply.github.com>
2024-05-18 19:39:52 +0300

    Update inverse_power_method.py

--- practicum_7/homework/basic/inverse_power_method.py
+++ practicum_7/homework/basic/inverse_power_method.py
@@ -7,20 +7,20 @@ NDArrayFloat = NDArray[np.float_]
 
 
 
-def inverse_power_method(A: NDArrayFloat, n_iters: int) -> NDArrayFloat:
+def inverse_power_method(A: NDArray, n_iters: int) -> NDArray:
     u_k = np.random.rand(A.shape[1])  # Создаем рандомный вектор
     u_k = u_k / np.linalg.norm(u_k)
 
-    history = []  # Список для сохранения истории минимальных собственных чисел
+    history = np.empty(n_iters, dtype=np.float64)  # Инициализируем массив для сохранения истории
 
-    for _ in range(n_iters):  # Следующий вектор
+    for i in range(n_iters):  # Следующий вектор
         pre_u_kk = np.linalg.solve(A, u_k)  # Решаем систему линейных уравнений Ax = u_k => x = A^(-1) * u_k
         A_u_k_norm = np.linalg.norm(pre_u_kk)
         u_k = pre_u_kk / A_u_k_norm
 
         # Вычисляем приближение к наименьшему по модулю собственному числу матрицы A
         eigenvalue = np.linalg.norm(np.dot(A, u_k)) / np.linalg.norm(u_k)  # ||Au_k|| / ||u_k||
-        history.append(eigenvalue)
+        history[i] = eigenvalue
 
     return history
 
